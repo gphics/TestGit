7aab5f8f111f9c740f333b0b0a417137d8842b19
gphics <97003589+gphics@users.noreply.github.com>
2022-12-11 19:22:44 -1100

    'first'

---
+++ build/precache-manifest.1136a3f650bbbb40c8409ddec74da655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52e2f9c529be9d41b02",
+    "url": "/static/css/main.182ed35c.chunk.css"
+  },
+  {
+    "revision": "a52e2f9c529be9d41b02",
+    "url": "/static/js/main.a52e2f9c.chunk.js"
+  },
+  {
+    "revision": "e97298fbcfc819364861",
+    "url": "/static/js/1.e97298fb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "f1d584fd6dce8ed4bd6a459488a6385e",
+    "url": "/index.html"
+  }
+];
